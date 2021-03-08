97c111b224bef818cb2d3b348bdd158346f6df82
F <vip.zatebe@mail.ru>
2021-03-08 20:41:07 +0300

    db_commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+# def main():
+#     db_session.global_init("db/main.db")
+
+
+@app.route("/")
+def index():
+    return render_template("common/base.html")
+
+
+if __name__ == '__main__':
+    #main()
+    app.run(port=6655)
