35c824fc6ac5c5e9dc31866cf72c8cba707ebfa7
F <vip.zatebe@mail.ru>
2021-03-10 14:19:50 +0300

    register_commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
+from Forms.user import RegisterForm
 from data import db_session
 from data.users import User
 
@@ -13,6 +14,31 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route("/")
 def index():
     return render_template("common/main_page.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('common/register_page.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('common/register_page.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('common/register_page.html', title='Регистрация', form=form)
+
+
 
 
 if __name__ == '__main__':
