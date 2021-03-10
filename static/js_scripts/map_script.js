50ba7767e52da1ad6af66376013a930f919439fe
F <vip.zatebe@mail.ru>
2021-03-10 18:19:22 +0300

    bug_fix_commit

---
+++ static/js_scripts/map_script.js
@@ -0,0 +1,7 @@
+ymaps.ready(init);
+function init(){
+            var myMap = new ymaps.Map("map", {
+                center: [51.661155, 39.207532],
+                zoom: 15
+            });
+        }
