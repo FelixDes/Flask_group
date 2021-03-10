22e386ae31faf3b0a5e964a0cad73aced84b830b
F <vip.zatebe@mail.ru>
2021-03-10 22:40:03 +0300

    bug_fix_commit

---
+++ Forms/user.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
