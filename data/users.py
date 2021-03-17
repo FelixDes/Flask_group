8ace51606b48f411e81d0be77de3302040f621a7
F <vip.zatebe@mail.ru>
2021-03-17 21:06:28 +0300

    db_commit_4

--- data/users.py
+++ data/users.py
@@ -2,6 +2,7 @@ import datetime
 
 import sqlalchemy
 from sqlalchemy.orm import relationship
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from .db_session import SqlAlchemyBase
 
@@ -9,7 +10,7 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    message = relationship("Message", back_populates="users")
+    messages = relationship("Message", back_populates="users")
 
     id = sqlalchemy.Column(sqlalchemy.Integer, unique=True, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
@@ -17,6 +18,15 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'<User> {self.id} {self.name} {self.email} {self.is_admin}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
 
 class Message(SqlAlchemyBase):
